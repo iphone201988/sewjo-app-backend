db3e88b2433530c27978ac8890f192d2cd3de9a5
devanshkbhardwaj <87934616+devanshkbhardwaj@users.noreply.github.com>
2024-11-12 16:52:42 +0530

    changes

--- models/fabric.model.js
+++ models/fabric.model.js
@@ -5,133 +5,166 @@ const fabricSchema = new mongoose.Schema(
     name: {
       type: String,
       //   required: true,
+      default: ""
     },
     description: {
       type: String,
+      default: ""
       //   required: true,
     },
     material: {
       type: String,
+      default: ""
       //   required: true,
     },
     width: {
       type: Number,
+      default: 0,
       //   required: true,
     },
     widthType: {
       type: String,
+      default: ""
       //   required: true,
     },
     weight: {
       type: Number,
+      default: 0
       //   required: true,
     },
     weightType: {
       type: String,
+      default: ""
     },
     color: {
       type: String,
+      default: ""
       //   required: true,
     },
     price: {
       type: Number,
+      default: 0
       //   required: true,
     },
     imageUrls: {
       type: [String],
+      default: []
       //   required: true,
     },
     userRef: {
       type: String,
+      default: ""
       //   required: true,
     },
     length: {
       type: Number,
+      default: ""
       //   require: true,
     },
     lenghtType: {
       type: String,
+      default: ""
       //   require: true,
     },
     quantity: {
       type: Number,
+      default: 0
       //   require: true,
     },
     weave: {
       type: String,
+      default: ""
       //   require: true,
     },
     usageIntent: {
       type: [String],
+      default: []
       //   require: true,
     },
     fabricName: {
       type: [String],
+      default: []
       //   require: true,
     },
     Pattern: {
       type: [String],
+      default: []
       //   require: true,
     },
     stretchtype: {
       type: String,
+      default: ""
       //   require: true,
     },
     stretchWidth: {
       type: Number,
+      default: 0
       // require: true,
     },
     stretchLength: {
       type: Number,
+      default: 0
       // require:true
     },
 
     lengthValue: {
       type: String,
+      default: ""
       // require:true
     },
     widthValue: {
       type: String,
+      default: ""
       // require:true
     },
 
     shrinkage: {
       type: String,
+      default: ""
       // require:true
     },
     drape: {
       type: String,
+      default: ""
       // require:true
     },
     imperfection: {
       type: String,
+      default: ""
       // require:true
     },
     shope: {
       type: String,
+      default: ""
       // require:true
     },
     currencyType: {
       type: String,
+      default: ""
       // require:true
     },
     currency: {
       type: Number,
+      default: 0
     },
     location: {
       type: String,
+      default: ""
       // require:true
     },
     note: {
       type: String,
+      default: ""
       // require:true
     },
     tags: {
       type: [String],
+      default: []
       // require:true
     },
     fabricContent: {
       type: [String],
+      default: []
     },
   },
   { timestamps: true }
